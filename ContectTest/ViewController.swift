a58346bbe69ec941c03347d0cfc8472c68947157
Samir Solanki <solanki.samir@gmail.com>
2016-12-27 15:34:51 +0530

    Initial Commit

---
+++ ContectTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContectTest
+//
+//  Created by Samir  on 27/12/16.
+//  Copyright © 2016 STL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
